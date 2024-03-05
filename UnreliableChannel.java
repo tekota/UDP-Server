e2007aa46c33dc47f4ff90fdd3f9e9b2e133c196
Ryan Khalil <rfk27@mail.aub.edu>
2024-03-05 22:40:44 +0200

    implemented the rnd generator for delay and loss

--- UnreliableChannel.java
+++ UnreliableChannel.java
@@ -29,8 +29,10 @@ public class UnreliableChannel {
 
         private int packetsRecievedA = 0;
         private int packetsRecievedB = 0;
-        private int packetsDropped = 0;
-        private int packetsDelayed = 0;
+        private int packetsDroppedA = 0;
+        private int packetsDroppedB = 0;
+        private int packetDelayedA = 0;
+        private int packetDelayedB = 0;
 
         private double totalDelayA = 0;
         private double totalDelayB = 0;
@@ -41,7 +43,7 @@ public class UnreliableChannel {
             this.maxDelay = maxDelay;
         }
 
-        public void listen(int port){
+        public void listen(int port) {
             try {
                 DatagramSocket ds = new DatagramSocket(port);
                 System.out.println("Server is listening on port " + port);
@@ -53,7 +55,7 @@ public class UnreliableChannel {
                     ds.receive(dpr);
 
                     // process the packet to determine if it should be dropped or delayed
-                    processPacket(dpr);
+                    rnd(dpr);
 
                 }
             } catch (IOException e) {
@@ -61,8 +63,54 @@ public class UnreliableChannel {
             }
         }
 
-        private void processPacket (DatagramPacket dpr){
-            
+        // function that simulates the packet drop and delay of the server
+        private void rnd(DatagramPacket dpr) {
+            byte[] data = dpr.getData();
+            String str = new String(data, 0, dpr.getLength());
+
+            // split the string to get the client info
+            String[] clientInfo = str.split(" ");
+            String client = clientInfo[0];
+
+            Random rnd = new Random();
+            double r = rnd.nextDouble();
+
+            // if the random number is less than p, the packet is dropped
+            if (r <= p) {
+                if (client.equals("A")) {
+                    packetsDroppedA++;
+                } else {
+                    packetsDroppedB++;
+                }
+                System.out.println(" the packet from client " + client + " was dropped: " + str); // to be removed later
+                return;
+
+            } else {
+                // if the random number is greater than p, the packet is delayed by a certain
+                // amount of time between mindDelay and maxDelay
+                long delay = (long) ((Math.random() * (maxDelay - minDelay + 1)) + minDelay);
+
+                // delaying the packet by putting the thread to sleep for the delay time
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                // send the packet to the client
+                if (client.equals("A")) {
+                    packetsRecievedA++; // increment the number of packets recieved by A
+                    packetDelayedA++; // increment the number of packets delayed by A
+                    totalDelayA += delay; // add the delay to the total delay of A
+                } else {
+                    packetsRecievedB++; // increment the number of packets recieved by B
+                    packetDelayedB++; // increment the number of packets delayed by B
+                    totalDelayB += delay; // add the delay to the total delay of B
+                }
+
+                
+
+            }
         }
     }
 }
