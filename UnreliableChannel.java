3d63edb31eec4905b9fd4ed7f394158716d3a3be
Ryan Khalil <rfk27@mail.aub.edu>
2024-03-12 20:52:08 +0200

    fixed the whole file ffs

--- UnreliableChannel.java
+++ UnreliableChannel.java
@@ -5,57 +5,69 @@ import java.net.InetAddress;
 import java.util.Random;
 
 public class UnreliableChannel {
-    // Path: Client.java <-- Hania
-    class Client {
-        private String name;
-        private DatagramSocket socket;
-        private InetAddress serverAddress;
-        private int serverPort;
-        private int packetCount = 0;
-        private int packetsReceivedCount = 0;
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(0.3, 0, 200, 5000);
+        Client A = new Client("A", "localhost", server.port);
+        Client B = new Client("B", "localhost", server.port);
 
-        public Client(String name, String serverAddress, int serverPort) throws Exception {
-            this.name = name;
-            this.socket = new DatagramSocket();
-            this.serverAddress = InetAddress.getByName(serverAddress);
-            this.serverPort = serverPort;
-        }
+        // Store client sockets for server
+        server.clientASocket = A.socket;
+        server.clientBSocket = B.socket;
 
-        // Method to send a packet
-        private void sendPacket(String message) throws IOException {
-            String packetData = name + " " + (this.name.equals("A") ? "B" : "A") + " " + packetCount;
-            byte[] buffer = packetData.getBytes();
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
-            socket.send(packet);
-            packetCount++;
-        }
+        Thread serverThread = new Thread(() -> server.listen());
+        serverThread.start();
 
-        // Method to receive a packet
-        private void receivePacket() throws IOException {
-            byte[] buffer = new byte[65535];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
+        A.run(1000);
+        B.run(1000);
+        server.getStats();
+    }
+}
 
-            String message = new String(packet.getData(), 0, packet.getLength());
-            if (!message.equals("END")) { // Check if it's not the end-of-session signal
-                packetsReceivedCount++;
-            }
-        }
+// Path: Client.java <-- Hania
+class Client {
+    public String name;
+    public DatagramSocket socket;
+    private InetAddress serverAddress;
+    private int serverPort;
+    private int packetCount = 0;
+    private int packetsReceivedCount = 0;
 
-        // Main execution loop for the client
-        public void run() throws Exception {
-            while (packetCount < 1000) { // Send a thousand packets
-                sendPacket("Data");
-                receivePacket();
-                Thread.sleep(500); // Enforce 0.5 seconds between transmissions
-            }
+    public Client(String name, String serverAddress, int serverPort) throws Exception {
+        this.name = name;
+        this.socket = new DatagramSocket();
+        this.serverAddress = InetAddress.getByName(serverAddress);
+        this.serverPort = serverPort;
+    }
 
-            // Send the end-of-session signal
-            sendPacket("END");
-            System.out.println(this.name + ": Packets received from other user: " + packetsReceivedCount);
+    private void sendPacket(String message) throws IOException {
+        String packetData = name + " " + (this.name.equals("A") ? "B" : "A") + " " + packetCount;
+        byte[] buffer = packetData.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+        socket.send(packet);
+        packetCount++;
+    }
+
+    private void receivePacket() throws IOException {
+        byte[] buffer = new byte[65535];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+
+        String message = new String(packet.getData(), 0, packet.getLength());
+        if (!message.equals("END")) {
+            packetsReceivedCount++;
         }
     }
 
+    public void run(int n) throws Exception {
+        while (packetCount < n) {
+            sendPacket("Data");
+            receivePacket();
+            // Thread.sleep(500);
+        }
+
+        sendPacket("END");
+       // System.out.println(this.name + ": Packets received from other user: " + packetsReceivedCount);
+    }
 }
 
 // Path: Server.java
@@ -63,6 +75,7 @@ class Server {
 
     private double p; // probability of packet loss
     private double minDelay, maxDelay;
+    public int port;
 
     private int packetsRecievedA = 0;
     private int packetsRecievedB = 0;
@@ -74,31 +87,36 @@ class Server {
     private double totalDelayA = 0;
     private double totalDelayB = 0;
 
+    public DatagramSocket clientASocket;
+    public DatagramSocket clientBSocket;
+
     // constructor
-    public Server(double p, double minDelay, double maxDelay) {
+    public Server(double p, double minDelay, double maxDelay, int port) {
         this.p = p;
         this.minDelay = minDelay;
         this.maxDelay = maxDelay;
+        this.port = port;
     }
 
-    public void listen(int port) {
-        try {
-            DatagramSocket ds = new DatagramSocket(port);
+    public void listen() {
+        try (DatagramSocket ds = new DatagramSocket(port)) {
             System.out.println("Server is listening on port " + port);
-
-            int clients = 0;
-            while (clients < 2) {
-                byte[] recieve = new byte[65535];
-                // creating a packet to recieve the data and storing it in recieve
-                DatagramPacket dpr = new DatagramPacket(recieve, recieve.length);
+            System.out.println("Sending and receiving packets...please wait.");
+            int clientsFinished = 0;
+            while (clientsFinished < 2) {
+                byte[] receive = new byte[65535];
+                DatagramPacket dpr = new DatagramPacket(receive, receive.length);
                 ds.receive(dpr);
-
-                // process the packet to determine if it should be dropped or delayed
+                // process the packet
                 rnd(dpr);
-
+                if (new String(dpr.getData(), 0, dpr.getLength()).equals("END")) {
+                    clientsFinished++;
+                }
             }
+            ds.close();
+            getStats();
         } catch (IOException e) {
-            System.out.println("Error in communicatin with server.");
+            System.out.println("Error in communication with server.");
             e.printStackTrace();
         }
     }
@@ -111,8 +129,8 @@ class Server {
         // split the string to get the client info
         String[] clientInfo = str.split(" ");
         String client = clientInfo[0];
-        String destination = clientInfo[1];
-        String message = clientInfo[2];
+        // String destination = clientInfo[1];
+        // String message = clientInfo[2];
 
         // increment the number of packets recieved by the client
         if (client.equals("A")) {
@@ -128,12 +146,10 @@ class Server {
         if (r <= p) {
             if (client.equals("A")) {
                 packetsDroppedA++;
-            } else {
+            } else if (client.equals("B")){
                 packetsDroppedB++;
             }
-            System.out.println(" the packet from client " + client + " was dropped: " + str); // to be removed later
-            return;
-
+            //System.out.println("The packet from client " + client + " was dropped");// to be removed later
         } else {
             // if the random number is greater than p, the packet is delayed by a certain
             // amount of time between mindDelay and maxDelay
@@ -151,24 +167,33 @@ class Server {
             if (client.equals("A")) {
                 packetDelayedA++; // increment the number of packets delayed by A
                 totalDelayA += delay; // add the delay to the total delay of A
-            } else {
+            } else if (client.equals("B")) {
                 packetsRecievedB++; // increment the number of packets recieved by B
                 packetDelayedB++; // increment the number of packets delayed by B
                 totalDelayB += delay; // add the delay to the total delay of B
             }
 
-            System.out.println("The packet from client " + client + " was delayed by " + delay + " ms."); // to be
+
+           // System.out.println("The packet from client " + client + " was delayed by " + delay + " ms."); // to be
                                                                                                           // removed
                                                                                                           // later
+        }
+        try {
+            String destinationClient = (client.equals("A") ? "B" : "A");
+            DatagramSocket destinationSocket = (destinationClient.equals("A") ? clientASocket : clientBSocket);
+            destinationSocket.send(dpr);
+        } catch (IOException e) {
+            System.err.println("Error forwarding packet to destination client.");
+            e.printStackTrace();
         }
     }
 
     public void getStats() {
-        System.out.println("Packets recieved from A: " + packetsRecievedA + " | " + "Lost: " + packetsDroppedA
-                + " | " + "Delayed: " + packetDelayedA);
-        System.out.println("Packets recieved from B: " + packetsRecievedB + " | " + "Lost: " + packetsDroppedB
+        System.out.println("Packets recieved from A: " + (packetsRecievedA-1) + " | " + "Lost: " + packetsDroppedA
+                + " | " + "Delayed: " + (packetDelayedA-1));
+        System.out.println("Packets recieved from B: " + (packetsRecievedA-1) + " | " + "Lost: " + (packetsDroppedB-1)
                 + " | " + "Delayed: " + packetDelayedB);
-        System.out.println("Average delay from A to B: " + totalDelayA / packetDelayedA + " ms.");
-        System.out.println("Average delay from B to A: " + totalDelayB / packetDelayedB + " ms.");
+        System.out.println("Average delay from A to B: " + String.format("%.2f", totalDelayA / packetDelayedA) + " ms.");
+        System.out.println("Average delay from B to A: " + String.format("%.2f", totalDelayB / packetDelayedB) + " ms.");
     }
 }
